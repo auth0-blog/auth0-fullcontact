76000190be3a5b5a879abae109b47b86fdea4515
Kim Maida <kim.maida@auth0.com>
2017-11-22 17:27:59 -0500

    Migrate to auth0.js v9 (remove Lock and replace with centralized login)

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
 window.addEventListener('load', function() {
-  var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN);
+  var webAuth = new auth0.WebAuth({
+    clientID: AUTH0_CLIENT_ID, 
+    domain: AUTH0_DOMAIN,
+    scope: 'openid profile email',
+    responseType: 'token',
+    redirectUri: AUTH0_CALLBACK_URL
+  });
 
   document.getElementById('btn-login').addEventListener('click', function() {
-    lock.show();
+    webAuth.authorize();
   });
 
   document.getElementById('btn-logout').addEventListener('click', function() {
     logout();
   });
 
-  lock.on("authenticated", function(authResult) {
-    lock.getProfile(authResult.idToken, function (err, profile) {
-      if (err) {
-        // Remove expired token (if any)
-        localStorage.removeItem('id_token');
-        // Remove expired profile (if any)
-        localStorage.removeItem('profile');
-        return alert('There was an error getting the profile: ' + err.message);
-      } else {
-        localStorage.setItem('id_token', authResult.idToken);
-        localStorage.setItem('profile', JSON.stringify(profile));
-        showUserProfile(profile);
-      }
-    });
+  webAuth.parseHash({ hash: window.location.hash }, (err, authResult) => {
+    if (err) {
+      return console.error(err);
+    }
+    if (authResult) {
+      webAuth.client.userInfo(authResult.accessToken, (err, profile) => {
+        if (err) {
+          // Remove expired token (if any)
+          localStorage.removeItem('token');
+          // Remove expired profile (if any)
+          localStorage.removeItem('profile');
+          return alert('There was an error getting the profile: ' + err.message);
+        } else {
+          localStorage.setItem('token', authResult.accessToken);
+          localStorage.setItem('profile', JSON.stringify(profile));
+          showUserProfile(profile);
+        }
+        window.location.hash = "";
+      });
+    }
   });
 
-  lock.on("authorization_error", function(error) {
-    lock.show({
-      flashMessage:{
-        type: 'error',
-        text: error.error_description
-      }
-    });
-  });
-
-  var parseHash = function() {
-    var id_token = localStorage.getItem('id_token');
-    if (null != id_token) {
+  var checkAuth = function() {
+    var token = localStorage.getItem('token');
+    if (token) {
       var user_profile = JSON.parse(localStorage.getItem('profile'));
       showUserProfile(user_profile);
     } // else: not authorized
@@ -52,10 +55,10 @@ window.addEventListener('load', function() {
   };
 
   var logout = function() {
-    localStorage.removeItem('id_token');
+    localStorage.removeItem('token');
     localStorage.removeItem('profile');
     window.location.href = "/";
   };
 
-  parseHash();
+  checkAuth();
 });
